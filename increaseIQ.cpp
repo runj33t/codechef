1284c956c65779e68f914910c2183d7abe9dd1ed
Badbravestone <ranjeetmaurya576@gmail.com>
2022-03-25 20:43:48 +0530

    increaseIQ

---
+++ increaseIQ.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n+7 > 170){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
