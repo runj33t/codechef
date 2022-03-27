dce78933cbcd9bcefcffc751db98fa97b2720f38
Badbravestone <ranjeetmaurya576@gmail.com>
2022-03-27 18:13:00 +0530

    TRD

---
+++ theRatingDilemma.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<string>
+#include<math.h>
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define tc(t) \
+    long long int t;    \
+    cin >> t; \
+    while (t--)
+#define loop(var, n) for (int var = 0; var < n; var++)
+#define ll long long int
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    n++;
+    cout<<-n<<endl;
+}
+int main()
+{
+    fast
+    tc(t)
+    {
+        solve();
+    }
+    return 0;
+}
